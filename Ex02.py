8627876fe0a040215fd2341bdc73cbec10c9bd48
ThiagodeRuijter <thiago.de.ruijter@student.howest.be>
2023-11-06 11:22:26 +0100

    oef 2 is half afgewerkt

--- Ex02.py
+++ Ex02.py
@@ -1,6 +1,22 @@
 # Ex02
 
 # schrijf hier jouw functie
+from typing import List
+from typing import Dict
+
+def maak_koppels(lijst_koppels:List[str], aantal_koppels:int) -> Dict[str,str]:
+    resultaat = {}
+    if aantal_koppels <= 4:
+
+        for naam in lijst_koppels:  
+            if naam not in resultaat.keys():
+                for naam2 in lijst_koppels:
+                    if naam2 not in resultaat.values():
+                        if naam2 not in resultaat.keys():
+                            resultaat[naam] = naam2
+
+
+    return resultaat
 
 
 
@@ -11,3 +27,4 @@ studenten = ["Karel", "Ben", "Tim", "Ken", "Henk", "Lies", "Barbie", "Sandra", "
 aantal_koppels = int(input("Hoeveel koppels moeten er gevormd worden?:> "))
 # ...
 
+koppels = print(f"De koppels van Howest zijn: \n{maak_koppels(studenten,aantal_koppels)}")
